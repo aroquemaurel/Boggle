557ee3b9f8bee8617ee6529d002f65208a184430
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2013-02-28 19:06:52 +0100

    Création projet netbeans

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: aroquemaurel
+ *
+ * Created on 28 février 2013, 19:00
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return (EXIT_SUCCESS);
+}
+
