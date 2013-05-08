c94eadf0b2ecf4b5cfed3dc70675d9785cf16ec9
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2013-05-08 16:53:26 +0200

    Config nbproject pour Cunit, sonar

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -82,47 +82,47 @@ boggle: ${OBJECTFILES}
 ${OBJECTDIR}/main.o: nbproject/Makefile-${CND_CONF}.mk main.c 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.c
+	$(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.c
 
 ${OBJECTDIR}/src/dictionnaire.o: nbproject/Makefile-${CND_CONF}.mk src/dictionnaire.c 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/dictionnaire.o src/dictionnaire.c
+	$(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/dictionnaire.o src/dictionnaire.c
 
 ${OBJECTDIR}/src/interfaceNcurses.o: nbproject/Makefile-${CND_CONF}.mk src/interfaceNcurses.c 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/interfaceNcurses.o src/interfaceNcurses.c
+	$(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/interfaceNcurses.o src/interfaceNcurses.c
 
 ${OBJECTDIR}/src/interfaceTexte.o: nbproject/Makefile-${CND_CONF}.mk src/interfaceTexte.c 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/interfaceTexte.o src/interfaceTexte.c
+	$(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/interfaceTexte.o src/interfaceTexte.c
 
 ${OBJECTDIR}/src/jeu.o: nbproject/Makefile-${CND_CONF}.mk src/jeu.c 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/jeu.o src/jeu.c
+	$(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/jeu.o src/jeu.c
 
 ${OBJECTDIR}/src/plateau.o: nbproject/Makefile-${CND_CONF}.mk src/plateau.c 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/plateau.o src/plateau.c
+	$(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/plateau.o src/plateau.c
 
 ${OBJECTDIR}/src/resolveur.o: nbproject/Makefile-${CND_CONF}.mk src/resolveur.c 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/resolveur.o src/resolveur.c
+	$(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/resolveur.o src/resolveur.c
 
 ${OBJECTDIR}/src/solution.o: nbproject/Makefile-${CND_CONF}.mk src/solution.c 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/solution.o src/solution.c
+	$(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/solution.o src/solution.c
 
 ${OBJECTDIR}/src/util.o: nbproject/Makefile-${CND_CONF}.mk src/util.c 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/util.o src/util.c
+	$(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/util.o src/util.c
 
 # Subprojects
 .build-subprojects:
@@ -153,31 +153,31 @@ tests/utilTests: ${TESTDIR}/tests/utilTests.o ${OBJECTFILES:%.o=%_nomain.o}
 ${TESTDIR}/tests/dicoTests.o: tests/dicoTests.c 
 	${MKDIR} -p ${TESTDIR}/tests
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -ICunit/CUnit/Headers -std=c99 -lncurses -lpanel -lmenu -LCunit/lib -lcunit `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/dicoTests.o tests/dicoTests.c
+	$(COMPILE.c) -g -Ilib -ICunit -ICunit/CUnit/Headers -std=c99 -lncurses -lpanel -lmenu -LCunit/lib -lcunit `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/dicoTests.o tests/dicoTests.c
 
 
 ${TESTDIR}/tests/jeuTest.o: tests/jeuTest.c 
 	${MKDIR} -p ${TESTDIR}/tests
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -ICunit/CUnit/Headers -std=c99 -lncurses -lpanel -lmenu -LCunit/lib -lcunit `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/jeuTest.o tests/jeuTest.c
+	$(COMPILE.c) -g -Ilib -ICunit -ICunit/CUnit/Headers -std=c99 -lncurses -lpanel -lmenu -LCunit/lib -lcunit `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/jeuTest.o tests/jeuTest.c
 
 
 ${TESTDIR}/tests/plateauTests.o: tests/plateauTests.c 
 	${MKDIR} -p ${TESTDIR}/tests
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -ICunit/CUnit/Headers -std=c99 -lncurses -lpanel -lmenu -LCunit/lib -lcunit `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/plateauTests.o tests/plateauTests.c
+	$(COMPILE.c) -g -Ilib -ICunit -ICunit/CUnit/Headers -std=c99 -lncurses -lpanel -lmenu -LCunit/lib -lcunit `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/plateauTests.o tests/plateauTests.c
 
 
 ${TESTDIR}/tests/solutionTests.o: tests/solutionTests.c 
 	${MKDIR} -p ${TESTDIR}/tests
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -ICunit/CUnit/Headers -std=c99 -lncurses -lpanel -lmenu -LCunit/lib -lcunit `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/solutionTests.o tests/solutionTests.c
+	$(COMPILE.c) -g -Ilib -ICunit -ICunit/CUnit/Headers -std=c99 -lncurses -lpanel -lmenu -LCunit/lib -lcunit `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/solutionTests.o tests/solutionTests.c
 
 
 ${TESTDIR}/tests/utilTests.o: tests/utilTests.c 
 	${MKDIR} -p ${TESTDIR}/tests
 	${RM} $@.d
-	$(COMPILE.c) -g -Ilib -ICunit/CUnit/Headers -std=c99 -lncurses -lpanel -lmenu -LCunit/lib -lcunit `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/utilTests.o tests/utilTests.c
+	$(COMPILE.c) -g -Ilib -ICunit -ICunit/CUnit/Headers -std=c99 -lncurses -lpanel -lmenu -LCunit/lib -lcunit `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/utilTests.o tests/utilTests.c
 
 
 ${OBJECTDIR}/main_nomain.o: ${OBJECTDIR}/main.o main.c 
@@ -188,7 +188,7 @@ ${OBJECTDIR}/main_nomain.o: ${OBJECTDIR}/main.o main.c
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/main_nomain.o main.c;\
+	    $(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/main_nomain.o main.c;\
 	else  \
 	    ${CP} ${OBJECTDIR}/main.o ${OBJECTDIR}/main_nomain.o;\
 	fi
@@ -201,7 +201,7 @@ ${OBJECTDIR}/src/dictionnaire_nomain.o: ${OBJECTDIR}/src/dictionnaire.o src/dict
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/dictionnaire_nomain.o src/dictionnaire.c;\
+	    $(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/dictionnaire_nomain.o src/dictionnaire.c;\
 	else  \
 	    ${CP} ${OBJECTDIR}/src/dictionnaire.o ${OBJECTDIR}/src/dictionnaire_nomain.o;\
 	fi
@@ -214,7 +214,7 @@ ${OBJECTDIR}/src/interfaceNcurses_nomain.o: ${OBJECTDIR}/src/interfaceNcurses.o 
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/interfaceNcurses_nomain.o src/interfaceNcurses.c;\
+	    $(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/interfaceNcurses_nomain.o src/interfaceNcurses.c;\
 	else  \
 	    ${CP} ${OBJECTDIR}/src/interfaceNcurses.o ${OBJECTDIR}/src/interfaceNcurses_nomain.o;\
 	fi
@@ -227,7 +227,7 @@ ${OBJECTDIR}/src/interfaceTexte_nomain.o: ${OBJECTDIR}/src/interfaceTexte.o src/
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/interfaceTexte_nomain.o src/interfaceTexte.c;\
+	    $(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/interfaceTexte_nomain.o src/interfaceTexte.c;\
 	else  \
 	    ${CP} ${OBJECTDIR}/src/interfaceTexte.o ${OBJECTDIR}/src/interfaceTexte_nomain.o;\
 	fi
@@ -240,7 +240,7 @@ ${OBJECTDIR}/src/jeu_nomain.o: ${OBJECTDIR}/src/jeu.o src/jeu.c
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/jeu_nomain.o src/jeu.c;\
+	    $(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/jeu_nomain.o src/jeu.c;\
 	else  \
 	    ${CP} ${OBJECTDIR}/src/jeu.o ${OBJECTDIR}/src/jeu_nomain.o;\
 	fi
@@ -253,7 +253,7 @@ ${OBJECTDIR}/src/plateau_nomain.o: ${OBJECTDIR}/src/plateau.o src/plateau.c
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/plateau_nomain.o src/plateau.c;\
+	    $(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/plateau_nomain.o src/plateau.c;\
 	else  \
 	    ${CP} ${OBJECTDIR}/src/plateau.o ${OBJECTDIR}/src/plateau_nomain.o;\
 	fi
@@ -266,7 +266,7 @@ ${OBJECTDIR}/src/resolveur_nomain.o: ${OBJECTDIR}/src/resolveur.o src/resolveur.
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/resolveur_nomain.o src/resolveur.c;\
+	    $(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/resolveur_nomain.o src/resolveur.c;\
 	else  \
 	    ${CP} ${OBJECTDIR}/src/resolveur.o ${OBJECTDIR}/src/resolveur_nomain.o;\
 	fi
@@ -279,7 +279,7 @@ ${OBJECTDIR}/src/solution_nomain.o: ${OBJECTDIR}/src/solution.o src/solution.c
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/solution_nomain.o src/solution.c;\
+	    $(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/solution_nomain.o src/solution.c;\
 	else  \
 	    ${CP} ${OBJECTDIR}/src/solution.o ${OBJECTDIR}/src/solution_nomain.o;\
 	fi
@@ -292,7 +292,7 @@ ${OBJECTDIR}/src/util_nomain.o: ${OBJECTDIR}/src/util.o src/util.c
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -Ilib -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/util_nomain.o src/util.c;\
+	    $(COMPILE.c) -g -Ilib -ICunit -std=c99 -lncurses -lpanel -lmenu -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/util_nomain.o src/util.c;\
 	else  \
 	    ${CP} ${OBJECTDIR}/src/util.o ${OBJECTDIR}/src/util_nomain.o;\
 	fi
